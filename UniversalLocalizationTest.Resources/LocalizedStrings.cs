be668daac26e9c19a25c25db6d1829d1b7d05ab1
Andreas Hammar <andreas.hammar@jayway.com>
2015-01-09 11:44:15 +0100

    added code-accessor

---
+++ UniversalLocalizationTest.Resources/LocalizedStrings.cs
@@ -0,0 +1,12 @@
+﻿using Windows.ApplicationModel.Resources;
+
+namespace UniversalLocalizationTest.Resources
+{
+    public class LocalizedStrings
+    {
+        public static string Get(string key)
+        {
+            return ResourceLoader.GetForCurrentView().GetString(key);
+        }
+    }
+}
--- UniversalLocalizationTest.Shared/MainPage.xaml.cs
+++ UniversalLocalizationTest.Shared/MainPage.xaml.cs
@@ -1,19 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
+using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+using UniversalLocalizationTest.Resources;
 
 namespace UniversalLocalizationTest
 {
@@ -43,6 +35,11 @@ namespace UniversalLocalizationTest
             // Windows.Phone.UI.Input.HardwareButtons.BackPressed event.
             // If you are using the NavigationHelper provided by some templates,
             // this event is handled for you.
+        }
+
+        private async void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            await new MessageDialog(LocalizedStrings.Get("ClickMessageText")).ShowAsync();
         }
     }
 }
